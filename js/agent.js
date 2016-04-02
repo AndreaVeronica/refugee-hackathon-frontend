3a0aeca106b1cb9433485c08e5399895d4889f73
TanLui <effishing@gmail.com>
2016-04-02 17:21:50 -0400

    commit agent

---
+++ js/agent.js
@@ -0,0 +1,9 @@
+
+$("button").click(function(){
+  /*  $.getJSON("data.json", function(result){
+        $.each(result, function(i, field){
+            $("div").append(field + " ");
+        });
+    });*/ console.log('button button');
+});
+
