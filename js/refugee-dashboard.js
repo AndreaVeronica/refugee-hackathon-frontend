6b112bf255517f457262b4abf8e63767db08db7c
Andrea Airall <andreaairall@gmail.com>
2016-04-02 21:20:31 -0400

    homepage

--- js/refugee-dashboard.js
+++ js/refugee-dashboard.js
@@ -4,7 +4,7 @@ var jim = {
 	email : "fake@gmail.com"
 }
 
-
+//functions for contact database
 function fillRefugeeCard(obj) {
 
 	$('.contact-cell p').eq(0).text(obj.name);
@@ -12,7 +12,7 @@ function fillRefugeeCard(obj) {
 	$('.contact-cell p').eq(2).text(obj.email);
 
 }
-
+a1
 function fillCaseManagerCard(obj) {
 
 	$('.contact-cell p').eq(3).text(obj.name);
@@ -28,3 +28,23 @@ function fillCoSponsorCard(obj) {
 	$('.contact-cell p').eq(8).text(obj.email);
 
 }
+
+
+//function for task list
+
+$('input').submit(function () {
+    if ($('input').val() !== '') {
+        var input_value = $('input').val();
+        $('ul').append('<li>' + input_value + '<rm>x</rm></li>');
+        //$('ul').append('<li>' + input_value '<a href="">x</a></li>');
+
+    };
+    $('input').val('');
+    return false;
+});
+
+$(document).on('click', 'rm', function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});‹
+
