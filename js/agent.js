deb3c55720071ca52cc72a2d2489822218d8f2fe
Jordan Rust <jordan.rust92@gmail.com>
2016-04-03 13:27:34 -0400

    changed styling

--- js/agent.js
+++ js/agent.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // $("button").click(function() {
         $.getJSON("https://api.myjson.com/bins/1gglc", function(result) {
             for (var i = 0; i < result.refugees.length; i++) {
-                $("ul").append('<li>' + result.refugees[i].name + "</li> ");
+                $("ul .reflist").append('<li>' + result.refugees[i].name + "</li> ");
             }
         });
     // });
